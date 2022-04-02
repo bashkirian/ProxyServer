65dbd8757027ef5416439d2d457739d1a30f95b2
Almaz Shagiev <exaggeratethismotion@gmail.com>
2022-04-02 13:41:11 +0500

    сервер-клиент
    позже добавлю прокси-сервер

---
+++ Server_/Server_.cpp
@@ -0,0 +1,17 @@
+﻿// это СЕРВЕР. программа 1
+#include "socket_functions.h"
+
+using namespace std;
+
+int main()
+{
+	Socket task_socket;
+	string packet;
+    while (!cin.eof())
+    {
+		string packet_part;
+		getline(cin, packet_part);
+		task_socket.SendPacketToClient(packet_part);
+	}
+	return 0; // в деструкторе сокет закроется сам
+}
